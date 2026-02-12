2445115fdcd284aeb50c70738639decf7e7cd1cb
Said Salas <saidsalastech@gmail.com>
2026-02-11 23:16:39 -0600

    nateava 0.05

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import { createClient } from './utils/server'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+export default async function Page() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
+  const { data: todos } = await supabase.from('todos').select()
+
   return (
-    <div></div>
-  );
+    <ul>
+      {todos?.map((todo) => (
+        <li>{todo}</li>
+      ))}
+    </ul>
+  )
 }
---
+++ notes.md
@@ -0,0 +1,14 @@
+What we fix:
+
+Invoices
+Inventory
+Cash closing
+Making orders
+Daily/Weekly/Monthly/Yearly Sales and profits
+Handling employees schedules
+Communicacion: make everyone know about any changes
+Food waste control
+Log book
+What do I need to know from the business?
+
+Make it end-to-end. Super easy. 
